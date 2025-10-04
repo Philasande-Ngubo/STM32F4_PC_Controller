455ea4f21dad7a79783478a318273c5725f5d2f4
Philasande Ngubo <pngubo041120@gmail.com>
2025-10-04 15:44:54 +0200

    Added the init LEDs code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -36,12 +36,11 @@ TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim3;
 DMA_HandleTypeDef hdma_tim2_ch1;
 
+//Methods
+void initLEDs();
+
 /* USER CODE BEGIN PV */
 // TODO: Add code for global variables, including LUTs
-
-
-
-
 
 /**
   * @brief  The application entry point.
@@ -51,27 +50,18 @@ int main(void)
 {
 
   HAL_Init();
-
+  initLEDs();
 
   while (1)
   {
     /* USER CODE END WHILE */
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-    GPIO_InitStruct.Pin = GPIO_PIN_3;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;  // Push-pull output
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
-    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
-
-
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
+}
+
+void initLEDs(){
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  GPIOB->MODER = 0x5555;
+  GPIOB->ODR = 0xFF;
 }
